e362ae41ffad6aefc6a14ad787237699f01e080c
Surya <s8296659@gmail.com>
2026-03-06 13:02:58 +0530

    Update web3utils.py

--- web3utils.py
+++ web3utils.py
@@ -1,5 +1,5 @@
 # web3utils.py
-import os, time
+import os
 from dotenv import load_dotenv
 from web3 import Web3
 
@@ -8,38 +8,18 @@ load_dotenv()
 RPC_HTTP = os.getenv("RPC_HTTP")
 RPC_WSS = os.getenv("RPC_WSS")
 CHAIN_ID = int(os.getenv("CHAIN_ID", "80001"))
-WALLET_ADDRESS = Web3.to_checksum_address(os.getenv("WALLET_ADDRESS"))
-PRIVATE_KEY = os.getenv("PRIVATE_KEY")
 
-w3_http = Web3(Web3.HTTPProvider(RPC_HTTP))
-w3_wss = Web3(Web3.WebsocketProvider(RPC_WSS)) if RPC_WSS else None
+w3 = Web3(Web3.HTTPProvider(RPC_HTTP))
+w3_ws = Web3(Web3.WebsocketProvider(RPC_WSS)) if RPC_WSS else None
 
-def check_connection():
-    if not w3_http.is_connected():
+def ensure_connected():
+    if not w3.is_connected():
         raise RuntimeError("HTTP RPC not connected")
-    if w3_wss and not w3_wss.is_connected():
-        print("Warning: WSS not connected; listener may be degraded")
+    if w3_ws and not w3_ws.is_connected():
+        print("Warning: WSS not connected")
 
-def to_wei_matic(amount_decimal):
-    return int(float(amount_decimal) * 10**18)
+def to_wei_matic(x):
+    return int(float(x) * 10**18)
 
-def from_wei_matic(value):
-    return float(value) / 10**18
-
-def build_tx(tx):
-    tx.setdefault("chainId", CHAIN_ID)
-    if "nonce" not in tx:
-        tx["nonce"] = w3_http.eth.get_transaction_count(WALLET_ADDRESS)
-    if "gas" not in tx:
-        try:
-            tx["gas"] = w3_http.eth.estimate_gas(tx)
-        except Exception:
-            tx["gas"] = int(os.getenv("MAX_TX_GAS", "500000"))
-    if "gasPrice" not in tx:
-        tx["gasPrice"] = w3_http.eth.gas_price
-    return tx
-
-def sign_and_send(tx):
-    signed = w3_http.eth.account.sign_transaction(tx, PRIVATE_KEY)
-    txhash = w3_http.eth.send_raw_transaction(signed.rawTransaction)
-    return txhash.hex()
+def from_wei_matic(x):
+    return float(x) / 10**18
